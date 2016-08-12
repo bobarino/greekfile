b8087415efd7e579d036be4141473d0ce99a1772
Nick Giammanco <ngiammanco@gmail.com>
2016-08-11 20:35:26 -0500

    Updating User information based on Stripe calls

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,22 +3,11 @@ class ChargesController < ApplicationController
   end
 
   def create
-    session[:current_user_id] = current_user.id
-    # Amount in cents
-    @amount = 500
 
     customer = Stripe::Customer.create(
       :plan => "std",
       :email => params[:email],
       :source  => params[:stripeToken]
-    )
-    p customer
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
     )
 
   rescue Stripe::CardError => e
--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -9,36 +9,38 @@ class HooksController < ApplicationController
       request.body.rewind
       data_json = JSON.parse request.body.read
 
-      p data_json['data']['object']['customer']
       p data_json['type']
 
       if data_json['type'] == "invoice.payment_succeeded"
-        make_active()
+        make_active(data_json['data']['object']['customer'])
       end
 
       if data_json['type'] == "invoice.payment_failed"
-        make_inactive()
+        make_inactive(data_json['data']['object']['customer'])
       end
 
       if data_json['type'] == "customer.created"
-        make_customer(data_json['data']['object']['id'])
+        make_customer(data_json['data']['object']['sources']['data'][0]['name'], data_json['data']['object']['id'])
       end
       render nothing: true, status: 200
     end
 
-    def make_active
-      user = User.find(session[:current_user_id])
-      user.username = "success"
+    def make_active(cust)
+      user = User.where(stripe_cust_id: cust).first
+      user.active = true
       user.save!
     end
 
-    def make_inactive
-      p "failure"
+    def make_inactive(cust)
+      user = User.where(stripe_cust_id: cust).first
+      user.active = false
+      user.save!
     end
 
-    def make_customer(id)
-      user = User.find(session[:current_user_id])
+    def make_customer(email, id)
+      user = User.where(email: email).first
       user.stripe_cust_id = id
+      user.active = true
       user.save!
     end
 end
